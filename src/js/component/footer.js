8f0973331c4bf076c14407fecc8c4d583e384110
AnoukRImola <anouk138@gmail.com>
2021-02-08 20:30:49 +0000

    primer trabajo de react

---
+++ src/js/component/footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export function Footer() {
+	return (
+		<div className="card" className="p-3 mb-2 bg-dark text-white">
+			<div className="card-body">
+				<footer className="blockquote-footer">
+					Copyright Your Website 2018
+					<cite title="Source Title">Source Title</cite>
+				</footer>
+			</div>
+		</div>
+	);
+}
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,25 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { Navbar } from "./navbar";
+import { Card } from "./card";
+import { Jumbotron } from "./jumbotron";
+import { Footer } from "./footer";
 
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container-fluid">
+			<Navbar />
+			<Jumbotron />
+			<div className="d-flex justify-content-center">
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+			</div>
+
+			<Footer />
 		</div>
 	);
 }
